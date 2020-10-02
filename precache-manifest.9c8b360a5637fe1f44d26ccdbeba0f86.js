e78dc2166e519eca2b93da47e21e77d6dc9034b2
Prithviraj <mouryaprithviraj@gmail.com>
2020-10-03 02:33:35 +0530

    Updates

---
+++ precache-manifest.9c8b360a5637fe1f44d26ccdbeba0f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22256a0b7295c0e7b1b23696005a5580",
+    "url": "/airbnb-amenity-detection-react/index.html"
+  },
+  {
+    "revision": "1762d7ecf844f9f2716f",
+    "url": "/airbnb-amenity-detection-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "62f7412d01175a4bc8cf",
+    "url": "/airbnb-amenity-detection-react/static/css/main.eaf2ba52.chunk.css"
+  },
+  {
+    "revision": "1762d7ecf844f9f2716f",
+    "url": "/airbnb-amenity-detection-react/static/js/2.f52955d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airbnb-amenity-detection-react/static/js/2.f52955d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f7412d01175a4bc8cf",
+    "url": "/airbnb-amenity-detection-react/static/js/main.7fd2fc41.chunk.js"
+  },
+  {
+    "revision": "88b333df818cca8030ed",
+    "url": "/airbnb-amenity-detection-react/static/js/runtime-main.2c733bbb.js"
+  }
+]);
